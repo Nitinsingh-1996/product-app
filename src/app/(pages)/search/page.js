410cb24fc643d15edfd4c8369013518b92950d0e
Nitin singh <nitin.singh@hestabit.in>
2024-08-15 22:06:26 +0530

    search work complete

--- src/app/(pages)/search/page.js
+++ src/app/(pages)/search/page.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import PropTypes from "prop-types";
 
@@ -25,24 +27,47 @@ export default async function SearchPage({ searchParams }) {
   }
 
   return (
-    <div>
-      <h1>Search Results for "{query}"</h1>
-      <div>
+    <div className="p-6 flex flex-col items-center justify-center h-screen">
+      <h1 className="text-xl font-semibold flex gap-4">
+        <Link
+          href="/"
+          className="bg-purple-800 py-2 px-8 flex rounded-md text-white font-semibold"
+        >
+          Back
+        </Link>
+        Search Results for "{query}"
+      </h1>
+      <div className="flex">
         {products.length ? (
           products.map((product) => (
-            <div key={product.id}>
-              <h2>{product.title}</h2>
-              <img
-                src={product.image}
-                alt={product.title}
-                style={{ width: "100px", height: "auto" }}
-              />
-              <p>Price: ${product.price}</p>
-              <p>{product.description}</p>
+            <div className="w-1/3 p-4" key={product.id}>
+              <div className="p-4 shadow-lg rounded-lg">
+                <div className=" bg-slate-100 rounded-md p-8">
+                  <span className="p-6 relative h-64 flex">
+                    <Image
+                      src={product.image}
+                      alt={product.title}
+                      layout="fill"
+                      objectFit="contain"
+                    />
+                  </span>
+                </div>
+                <h2 className="text-lg font-medium my-3">{product.title}</h2>
+                <p>Price: ${product.price}</p>
+                <p>{product.description}</p>
+              </div>
             </div>
           ))
         ) : (
-          <p>No products found.</p>
+          <>
+            <p>No products found.</p>
+            <Link
+              href="/"
+              className="bg-purple-800 py-2 px-8 flex rounded-md text-white font-semibold"
+            >
+              Go back to Home
+            </Link>
+          </>
         )}
       </div>
     </div>
